969cf9b173884c87aebd292138424d341e4a5565
Mrxfg <mrxmrxow@gmail.com>
2026-03-04 12:22:52 +0300

    feat(frontend): connect frontend to items API

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      '/items': {
+        target: 'http://10.93.25.213:42002',
+        changeOrigin: true,
+      },
+    },
+  },
 })
